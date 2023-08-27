744da3bf6eb8f0130df720df7202d30cf1e5ee1d
Can <can.kayikci@ug.bilkent.edu.tr>
2023-08-27 22:49:56 +0300

    Routing to flight.ejs
    Routing to a specific page with flight data.

--- Airport/Server/app.js
+++ Airport/Server/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
-app.use((req,res,next) => {
+app.get('/',(req,res,next) => {
     //Write a code that will get data from database and send it to the client
     
     db.query('SELECT * FROM flights', (err, result, fields) => {
@@ -17,6 +17,17 @@ app.use((req,res,next) => {
         res.render('main', {flights: result});
     }
     );
+});
+
+app.post('/flights/:id', (req,res,next) => {
+    //console.log(req.params.id);
+
+    db.query('SELECT * FROM flights WHERE id = ?', [req.params.id], (err, result, fields) => {
+        if(err) throw err;
+        console.log(result);
+        res.render('flight', {flight: result[0]}
+        );
+});
 });
 
 app.listen(3000);
